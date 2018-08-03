4cd4290da60fe1bd22eb5f5aeedd7abf74be5c1c
Chris Bailey <Chris.bailey@erlang-solutions.com>
2018-08-03 11:31:35 +0100

    Modified newborn_server to spawn nodes

--- apps/newborn/src/newborn_server.erl
+++ apps/newborn/src/newborn_server.erl
@@ -7,11 +7,10 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 
-
+-define(NODECOUNT, 5).
 %%%=============================================================================
 %% API functions
 %%%=============================================================================
-
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -22,7 +21,11 @@ start_link() ->
 %% @hidden
 init([]) ->
   {ok, _Pid} = net_kernel:start(['newborn@127.0.0.1', longnames]),
-  pong = net_adm:ping('mom@127.0.0.1'),
+  pong = net_adm:ping('newborn@127.0.0.1'),
+  NodeHost = "127.0.0.1",
+  NodeName = "newborn_",
+  NodeOpts = "-setcookie " ++ atom_to_list(erlang:get_cookie()),
+  [slave:start(NodeHost, NodeName ++ integer_to_list(N), NodeOpts) || N <- lists:seq(1, ?NODECOUNT)],
   {ok, no_state}.
 
 %% @hidden
