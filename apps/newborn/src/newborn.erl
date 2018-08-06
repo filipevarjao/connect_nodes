60482638e39bde70c0f5daa1a44463283f1aaded
bryan <bryan.hunt@erlang-solutions.com>
2018-08-06 16:30:08 +0100

    set up a counter via admin - log counter value

--- apps/newborn/src/newborn.erl
+++ apps/newborn/src/newborn.erl
@@ -4,6 +4,6 @@
 
 hello_world() ->
     Start = erlang:system_time(),
-    lager:log(error,self(),"logging a message "),  
+    lager:log(error,self(),"current metric : ~p " , [exometer:get_value([ newborn, hello_world ]) ]),  
     exometer:update_or_create([newborn,hello_world], erlang:system_time() - Start),
 ok.
